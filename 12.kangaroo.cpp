ee0e17285fcd7dccad6c6f42028d37a4d97635ba
htmos6 <88316097+htmos6@users.noreply.github.com>
2022-07-16 00:40:21 +0300

    Create 12.kangaroo.cpp

---
+++ 12.kangaroo.cpp
@@ -0,0 +1,10 @@
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    
+    if ((v2 < v1) && ((x1 - x2) % (v2 - v1) == 0))  {
+        return("YES");
+    }
+    else {
+        return("NO");
+    }
+}
