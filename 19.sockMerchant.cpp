952fc51d664db02da221198d50c44b06d1134f3d
htmos6 <88316097+htmos6@users.noreply.github.com>
2022-07-17 20:13:54 +0300

    Create 19.sockMerchant.cpp

---
+++ 19.sockMerchant.cpp
@@ -0,0 +1,16 @@
+int sockMerchant(int n, vector<int> ar) {
+
+    vector <int> socks(100, 0); 
+    
+    int ct = 0;
+    
+    for (int i = 0; i < ar.size() ; i++) {
+        socks[ar[i]-1] += 1;
+    }
+    
+    for (auto i = socks.begin() ; i < socks.end() ; i++ ) {
+        ct += *i / 2;
+    }
+    return  ct;
+
+}
