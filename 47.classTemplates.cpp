fe9c4b0a840195ec0e68932e3f55df90c143040f
htmos6 <88316097+htmos6@users.noreply.github.com>
2023-03-20 09:02:01 +0300

    Create 47.classTemplates.cpp

---
+++ 47.classTemplates.cpp
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <class T>
+class AddElements
+{
+public:
+    T result;
+    AddElements(T item1);
+    T add(T item2);
+    string concatenate(T item3);
+    
+};
+
+template <class T>
+AddElements<T>::AddElements(T item1)
+{
+    result = item1;    
+}
+
+template <class T>
+T AddElements<T>::add(T item2)
+{
+    result = result + item2;
+    return result;
+}
+
+template <class T>
+string AddElements<T>::concatenate(T item3)
+{
+    result = result + item3;
+    return result;
+}
+
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
